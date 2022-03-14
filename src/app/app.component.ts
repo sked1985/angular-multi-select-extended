e4ef65dc78499af356b9d1df827fd62b5adc8368
sked1985 <mcgettiganbarry@yahoo.com>
2022-03-14 14:42:26 +0000

    Commiting the initail setup for this

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
- 
+export class AppComponent implements OnInit {
+  formGroup: FormGroup;
+  pdList = [
+    'application',
+    'zipped',
+    'xrays',
+    'billing',
+    'finances',
+    'queues',
+    'programming'
+  ];
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.formGroup = this.fb.group({
+      pd: new FormControl([], Validators.required)
+    });
+      this.change({value: []})
+
+  }
+
+  change(event) {
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { MaterialExampleModule } from '../material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports: [ 
+    BrowserModule, 
+    FormsModule,   
+    MaterialExampleModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule
+  ],
+  declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
